87e0b4019e78757d02d1fb631836038ab2c6ea82
tommyyeh0505 <32403423+tommyyeh0505@users.noreply.github.com>
2018-12-26 20:56:39 -0800

    Added Main Menu UI
    Note:
    1. All Levels point to same method
    2. Difficulty not wired to anything
    3. PlayGame() in MainMenu.cs chooses next scene in order

---
+++ Cuphead/Assets/CupheadGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CupheadGame : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+      
+    }
+
+
+}
