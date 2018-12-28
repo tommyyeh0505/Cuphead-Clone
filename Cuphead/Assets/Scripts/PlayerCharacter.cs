97dfae54dd04926402e0e150d06f56b8f17c6deb
jiahuahl <jiahuahlee@hotmail.com>
2018-12-27 21:21:01 -0800

    Various tweeks: added direction facing for character. added deadzone for controller. set bullet speed / fire rate. set up level collision. update playerCharacter. PlayerCharacter.cs to take direction from PlayerMovementComponent. Fix hold down fire button.

--- Cuphead/Assets/Scripts/PlayerCharacter.cs
+++ Cuphead/Assets/Scripts/PlayerCharacter.cs
@@ -4,39 +4,30 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-
     public GameObject bullet;
     Vector2 bulletPos;
     public float fireRate = 0.5f;
     public float nextFire = 0.0f;
     bool facingRight = true;
+
+    private PlayerMovementComponent movementComponent;
     // Start is called before the first frame update
     void Start()
     {
-
+        movementComponent = gameObject.GetComponent<PlayerMovementComponent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Fires when Fire1 button is pressed or held down
-        if ((Input.GetButtonDown("Fire1") || Input.GetKey(KeyCode.LeftControl)) && Time.time > nextFire)
+        if ((Input.GetButtonDown("Fire1") || Input.GetButton("Fire1") && Time.time > nextFire))
         {
             nextFire = Time.time + fireRate;
             fire();
         }
-        float horizontalAxis = Input.GetAxisRaw("Horizontal");
-        if (horizontalAxis != 0)
-        {
-            if (horizontalAxis < 0)
-            {
-                facingRight = false;
-            }
-            else
-            {
-                facingRight = true;
-            }
-        }
+
+        facingRight = movementComponent.isFacingRight;
     }
 
     void OnCollisionEnter2D(Collision2D col)
--- Cuphead/Assets/Scripts/PlayerMovementComponent.cs
+++ Cuphead/Assets/Scripts/PlayerMovementComponent.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerMovementComponent : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpMaxHeight;
-    public float jumpMaxDuration;
-    public float jumpLingerTime;
-    public float gravity;
+    public float moveSpeed; // constant move speed
+    public float jumpMaxHeight; // apex of the jump
+    public float jumpMaxDuration; // time it takes to reach the apex
+    public float jumpLingerTime; // time after jump apex to retain jumping logic (before going into falling logic)
+    public float gravity; // parabolic falling coefficient
+
+    public float controllerDeadzone;
+
+    [HideInInspector] public bool isFacingRight;
 
     [HideInInspector] private bool hasJumped;
     [HideInInspector] private float verticalSpeed;
@@ -31,6 +35,7 @@ public class PlayerMovementComponent : MonoBehaviour
         currentJumpDuration = 0;
         verticalSpeed = 0;
 
+        // Precalculate some values so we don't have to do it every frame
         jumpSinWavePeriod = Mathf.PI / (2 * jumpMaxDuration);
         initialVerticalSpeed = jumpSinWavePeriod * jumpMaxHeight;
     }
@@ -53,12 +58,14 @@ public class PlayerMovementComponent : MonoBehaviour
         }
         else if (!JumpHeld || !hasJumped || currentJumpDuration > jumpMaxDuration + jumpLingerTime) // situation 2
         {
-            // if we get in situation 2, we can never get out until we hit ground
+            // if we get in situation 2, we can never get out until we hit ground           
             hasJumped = false;
+            // We use a parabolic curve to model the fall. Note that we linger with the sin curve for a little bit before falling
             verticalSpeed = Mathf.Min(verticalSpeed - (gravity * Time.deltaTime), -initialVerticalSpeed);
         }
         else // situation 3
         {
+            // We use a sin curve to model the jump upwards
             currentJumpDuration += Time.deltaTime;
             verticalSpeed = initialVerticalSpeed * Mathf.Cos(jumpSinWavePeriod * currentJumpDuration);
         }
@@ -97,6 +104,10 @@ public class PlayerMovementComponent : MonoBehaviour
 
     Vector2 ApplyHorizontalMovement(float horizontalAxis, Vector2 CurrentVector)
     {
+        isFacingRight = horizontalAxis > 0f;
+        Vector3 currentScale = gameObject.transform.localScale;
+        gameObject.transform.localScale = new Vector3(horizontalAxis * Mathf.Abs(currentScale.x), currentScale.y, currentScale.z);
+
         Bounds bounds = boxCollider.bounds;
         float distance = horizontalAxis * moveSpeed * Time.deltaTime;
         RaycastHit2D hit = Physics2D.BoxCast(CurrentVector, bounds.size, 0f, new Vector2(1f, 0f), distance, environmentLayer);
@@ -118,13 +129,17 @@ public class PlayerMovementComponent : MonoBehaviour
     {
         // Input.GetAxisRaw("Vertical");
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        Vector2 NewPosition = boxCollider.transform.position;
+        if (Mathf.Abs(horizontalAxis) > controllerDeadzone)
+        {
+            NewPosition = ApplyHorizontalMovement(Mathf.Sign(horizontalAxis), NewPosition);
+        }
 
         bool jumpButtionDown = Input.GetButtonDown("Jump");
         bool jumpButtionHeld = Input.GetButton("Jump");
 
-        Vector2 WithHorizontal = ApplyHorizontalMovement(horizontalAxis, rigidBody2D.position);
-        Vector2 WithHorizontalAndVertical = ApplyVerticalMovement(jumpButtionDown, jumpButtionHeld, WithHorizontal);
+        NewPosition = ApplyVerticalMovement(jumpButtionDown, jumpButtionHeld, NewPosition);
 
-        rigidBody2D.MovePosition(WithHorizontalAndVertical);
+        rigidBody2D.MovePosition(NewPosition);
     }
 }
