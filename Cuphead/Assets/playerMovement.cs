ff63a429c210ea50c568faf6f93e3e2e18df3858
QianzhenGoh <gohqianzhen@hotmail.com>
2018-12-27 17:47:00 -0800

    Added Sprites
    Background and Player Animation

---
+++ Cuphead/Assets/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour 
+{
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private Rigidbody2D rb2d;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement);
+    }
+}
