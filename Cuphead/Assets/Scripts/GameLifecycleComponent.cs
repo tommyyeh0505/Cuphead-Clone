3f9bbd39d64690270513f790a50908fe23a53d06
tommyyeh0505 <32403423+tommyyeh0505@users.noreply.github.com>
2018-12-29 11:32:05 -0800

    Added Gameover Overlay and GameOver Timer
    no game clear overlay and time yet

---
+++ Cuphead/Assets/Scripts/GameLifecycleComponent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameLifecycleComponent : MonoBehaviour
+{
+    private float initializationTime;
+    float levelTime;
+    [SerializeField] GameObject player; //used to check if player has died
+
+    //Game UI
+    [SerializeField] TMP_Text time;
+
+    //Gameover Overlay UI
+    [SerializeField] GameObject gameOverUI;
+    [SerializeField] TMP_Text gameOverTime;
+    [SerializeField] TMP_Text gameOverScore;
+    Boolean gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initializationTime = Time.timeSinceLevelLoad;
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            if (player == null)
+            {
+                gameOver = true;
+                gameOverTime.text = time.text;
+                gameOverUI.SetActive(true);
+            }
+            else
+            {
+                levelTime = Time.timeSinceLevelLoad - initializationTime;
+                TimeSpan t = TimeSpan.FromSeconds(levelTime);
+                time.text = t.ToString("mm':'ss':'ff");
+            }
+        }
+    }
+}
---
+++ Cuphead/Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
